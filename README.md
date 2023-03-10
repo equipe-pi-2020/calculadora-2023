f72cb000196c3955ddbb2e0dfa97e2157c5149b0
Ferdinando Prates <ferdinando.prates@intecomlogistica.com.br>
2023-03-10 09:15:34 -0300

    inicio

---
+++ README.md
@@ -0,0 +1,34 @@
+<img width=100% src="https://capsule-render.vercel.app/api?type=waving&color=029ef2&height=150&section=header&text=MINERVA&fontSize=60&fontColor=000000"/>
+
+### Integrantes 
+Antonio Cesar Bispo Miranda Filho<p>
+Cesar Inui<p>
+Dominique Karla Vieira<p>
+Ferdinando Martins Prates<p>
+Giovanni Garcia<p>
+Gisele Paiva Leite<p>
+Thaís Moura de Oliveira<p>
+
+### Objetivo da aplicação
+Auxiliar trabalhadores autônomos no quesito de planejamento financeiro, baseado nos seguintes inputs:<p> 
+•	Quanto eu quero ganhar por mês?<p>
+•	Quantos dias quero trabalhar por semana?<p>
+•	Quantas horas quero trabalhar por dia?<p>
+•	Quantas horas quero trabalhar por dia?<p>
+
+Recebendo como output o custo do seu trabalho por hora e o valor total do projeto.
+
+### Linguagens utilizadas 
+Javascript. Foi utilizada também a linguagem de marcação HTML e linguagem de design gráfico CSS.
+
+### Como testar a aplicação? 
+Para executar a aplicação localmente, basta clonar o seguinte repositório:
+https://github.com/equipe-pi-2020/calculadora
+em seguida clique no index.html
+
+Caso não opte por executar localmente, disponibilizamos abaixo um link usando o servidor do GitHub:
+https://equipe-pi-2020.github.io/calculadora/
+
+### Vídeo Tutorial
+
+[Link](https://github.com/equipe-pi-2020/calculadora/issues/7#issue-1369149525)
---
+++ app.js
@@ -0,0 +1,113 @@
+var nomeJob = document.getElementById("nomeJob")
+var result = document.querySelector('#result');
+var selectTable = document.querySelector('#selectTable');
+var table = document.querySelector('#table');
+
+var valorMes = document.getElementById("valorMes")
+var valorHorasDia = document.getElementById("valorHorasDia")
+var valorSemana = document.getElementById("valorSemana")
+var horasJob = document.getElementById("horasJob")              
+
+var valorHoraTrabalho = document.getElementById("valorHoraTrabalho")
+var valorJob = document.getElementById("ValorJob")
+
+function abrirTelaCalculadora() {
+  window.location.href = "calculator.html"
+}
+
+// Calcula valor das horas trabalhadas
+function calcularValorHorasTrabalhadas(valorMes,valorHorasDia,valorSemana ){
+  let valorHora = valorMes.value /  valorHorasDia.value / valorSemana.value
+  return valorHora.toFixed(2)
+}
+
+// Calcula o valor do projeto input ValorJob
+function calcularValorDoProjeto(valorHora, horasJob){
+  calcularValorHorasTrabalhadas(valorMes,valorHorasDia,valorSemana)
+  let resultado = valorHora    * horasJob.value
+  return resultado.toFixed(2).replace(".", ",")
+}
+
+// Mostra resultado nos inputs valorHoraTrabalho e ValorJob ao 
+// pressiona o botao CALCULAR 
+function mostrarResultado() {
+  if (
+    valorMes.value == '' ||
+    valorHorasDia.value == '' ||
+    valorSemana.value == '' ||
+    horasJob.value == ''
+  ) {
+    swal("🤭OPS!", "Por favor, preencha todos os campos antes de clicar em CALCULAR.");
+  } else {
+    valorHoraTrabalho.value = calcularValorHorasTrabalhadas(
+    valorMes,
+    valorHorasDia,
+    valorSemana
+    ).replace(".", ",");
+
+    valorJob.value = calcularValorDoProjeto(
+    calcularValorHorasTrabalhadas(valorMes,valorHorasDia,valorSemana),horasJob)
+    result.innerText = `Valor do projeto ${nomeJob.value.toUpperCase()} é de:`;
+    // Pergunta para adição de Freelance na tabela
+    selectTable.innerHTML = '<br><br>' +
+      `Deseja incluir ${nomeJob.value.toUpperCase()} na tabela?`
+      + '<div><button id ="addDep" value=" " onclick = "add()" >SIM</button>' 
+      + '<button id="notDep" onclick="window.location.reload()">NÃO</button></div>';
+  }
+}
+
+var tabela = table.innerHTML = `
+<table id = "dsTable" class="selectTable">
+  <thead>
+    <tr>
+      <th class="idNumber">Id</th>
+      <th>Freelance</th>
+      <th>Valor</th>
+      <th>Excluir</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Calculadora Freelance</td>
+      <td>R$ 3000,00</td>
+      <td> <input type="button" id ="deleteDep" value="excluir" class="buttonExcluir" onclick = "deleteRow(this)">  </td>
+    </tr>
+ `
+
+function add() {
+  
+  var linha = table.innerHTML = `
+      
+      <tr>
+        <td>2</td>
+        <td>${nomeJob.value}</td>
+        <td>R$ ${valorJob.value}</td>
+        <td> <input type="button" id ="deleteDep" value="excluir" class="buttonExcluir" onclick = "deleteRow(this)"  </td>
+      </tr>
+      </tbody>
+      </table>
+  `
+  document.getElementById("table").innerHTML = tabela + linha
+}
+
+function deleteRow(row){
+  var d = row.parentNode.parentNode.rowIndex;
+  document.getElementById("dsTable").deleteRow(d);
+  }
+
+// Funções do menu mobile
+
+function abre() {
+  document.getElementById("menu-mobile").style.right = `${'0'}`
+}
+
+function fechar() {
+  document.getElementById("menu-mobile").style.right = `${'-100vw'}`
+}
+
+// Carregamento da load-page
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var estilo = document.getElementsByClassName('load-page');
+  estilo[0].style.visibility = "hidden";
+});
